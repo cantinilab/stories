9607186cc048dedb3929dc546415a532ccf1d2de
Geert-Jan Huizing <huizing@ens.fr>
2024-07-08 15:55:39 +0200

    Add more tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,22 @@
 import stories
+import stories.steps
 
 
-def test_model():
+def test_model_explicit():
+    step = stories.steps.ExplicitStep()
+    model = stories.SpaceTime()
+
+
+def test_model_linear():
+    step = stories.steps.ExplicitStep()
+    model = stories.SpaceTime(quadratic=False)
+
+
+def test_model_ICNN_implicit():
+    step = stories.steps.ICNNImplicitStep()
+    model = stories.SpaceTime()
+
+
+def test_model_monge_implicit():
+    step = stories.steps.MongeImplicitStep()
     model = stories.SpaceTime()
