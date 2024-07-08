68210ee780f793baf646e17b83bdadad10374540
Geert-Jan Huizing <huizing@ens.fr>
2024-07-08 16:16:56 +0200

    np array in test

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,7 @@ import stories
 import stories.steps
 import anndata as ad
 import jax
-import jax.numpy as jnp
+import numpy as np
 
 key = jax.random.PRNGKey(0)
 key_0, key_1, key_2 = jax.random.split(key, 3)
@@ -11,11 +11,11 @@ x_0 = jax.random.normal(key_0, shape=(100, 20))
 x_1 = jax.random.normal(key_1, shape=(100, 20))
 x_2 = jax.random.normal(key_2, shape=(100, 20))
 
-adata_0 = ad.AnnData(x_0)
+adata_0 = ad.AnnData(np.array(x_0))
 adata_0.obs["time"] = 0
-adata_1 = ad.AnnData(x_1)
+adata_1 = ad.AnnData(np.array(x_1))
 adata_1.obs["time"] = 1
-adata_2 = ad.AnnData(x_2)
+adata_2 = ad.AnnData(np.array(x_2))
 adata_2.obs["time"] = 2
 
 adata = ad.concat((adata_0, adata_1, adata_2))
